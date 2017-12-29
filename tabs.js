f3531478ee52901246a5a80b943a882e02f3e889
Lorenzsj <lorenzsj@clarkson.edu>
2017-12-29 02:21:31 -0500

    good jumpoff point

--- tabs.js
+++ tabs.js
@@ -2,17 +2,16 @@ function listTabs() {
   getCurrentWindowTabs().then((tabs) => {
     let tabsList = document.getElementById('tabs-list');
     let currentTabs = document.createDocumentFragment();
-    let limit = 5;
+    let limit = 5; // change max tab limit here
     let counter = 0;
 
     tabsList.textContent = '';
 
     for (let tab of tabs) {
-      if (!tab.active && counter <= limit) {
+      if (counter <= limit) {
         let tabLink = document.createElement('a');
 
         tabLink.textContent = tab.title || tab.id;
-        tabLink.setAttribute('href', tab.id);
         tabLink.classList.add('switch-tabs');
         currentTabs.appendChild(tabLink);
       }
@@ -30,6 +29,7 @@ function getCurrentWindowTabs() {
   return browser.tabs.query({currentWindow: true});
 }
 
+/* handle click events in plugin box */
 document.addEventListener("click", (e) => {
   function callOnActiveTab(callback) {
     getCurrentWindowTabs().then((tabs) => {
@@ -39,36 +39,35 @@ document.addEventListener("click", (e) => {
         }
       }
     });
-}
-
-  if (e.target.id === "tabs-remove") {
-    callOnActiveTab((tab) => {
-      browser.tabs.remove(tab.id);
-    });
   }
 
-  else if (e.target.classList.contains('switch-tabs')) {
-    var tabId = +e.target.getAttribute('href');
+  if (e.target.id === "tabs-remove") {
+    massDelete();
+  }
+});
 
+function massDelete() {
+    var blacklist = ["4chan.org", "twitch.tv"]; // load this from file or somethhing
+
+    // line 55 is a condition that determine what tabs will be pulled into the 'tabs' variable
     browser.tabs.query({
       currentWindow: true
     }).then((tabs) => {
       for (var tab of tabs) {
-        if (tab.id === tabId) {
-          browser.tabs.update(tabId, {
-              active: true
-          });
-        }
+        blacklist.forEach(function(site) {
+          if (tab.url.indexOf(site) !== -1) {
+            browser.tabs.remove(tab.id);
+          }
+        });
       }
     });
   }
 
   e.preventDefault();
-});
 
 //onRemoved listener. fired when tab is removed
 browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
-  console.log(`The tab with id: ${tabId}, is closing`);
+  c4onsole.log(`The tab with id: ${tabId}, is closing`);
 
   if(removeInfo.isWindowClosing) {
     console.log(`Its window is also closing.`);
