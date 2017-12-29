fe573e7527c546f29bab17e60e8036063babafd8
Lorenzsj <lorenzsj@clarkson.edu>
2017-12-29 02:33:34 -0500

    working proof of concept

--- tabs.js
+++ tabs.js
@@ -1,8 +1,12 @@
+function getCurrentWindowTabs() {
+  return browser.tabs.query({currentWindow: true});
+}
+
 function listTabs() {
   getCurrentWindowTabs().then((tabs) => {
     let tabsList = document.getElementById('tabs-list');
     let currentTabs = document.createDocumentFragment();
-    let limit = 5; // change max tab limit here
+    let limit = tabs.length; // change max list tab limit here
     let counter = 0;
 
     tabsList.textContent = '';
@@ -23,11 +27,8 @@ function listTabs() {
   });
 }
 
+/* this will load the tab list everytime the plugin is opened */
 document.addEventListener("DOMContentLoaded", listTabs);
-
-function getCurrentWindowTabs() {
-  return browser.tabs.query({currentWindow: true});
-}
 
 /* handle click events in plugin box */
 document.addEventListener("click", (e) => {
@@ -44,26 +45,28 @@ document.addEventListener("click", (e) => {
   if (e.target.id === "tabs-remove") {
     massDelete();
   }
+
+  e.preventDefault();
 });
 
 function massDelete() {
-    var blacklist = ["4chan.org", "twitch.tv"]; // load this from file or somethhing
+  var blacklist = ["4chan.org", "twitch.tv"]; // load this from file or somethhing
 
-    // line 55 is a condition that determine what tabs will be pulled into the 'tabs' variable
-    browser.tabs.query({
-      currentWindow: true
-    }).then((tabs) => {
-      for (var tab of tabs) {
-        blacklist.forEach(function(site) {
-          if (tab.url.indexOf(site) !== -1) {
-            browser.tabs.remove(tab.id);
-          }
-        });
-      }
-    });
-  }
-
-  e.preventDefault();
+  /* get all tabs that are in the current window of firefox */
+  browser.tabs.query({
+    currentWindow: true
+  }).then((tabs) => {
+    /* go through all tabs */
+    for (var tab of tabs) {
+      /* check if the current tab has a substring from the blacklist array */
+      blacklist.forEach(function(site) {
+        if (tab.url.indexOf(site) !== -1) {
+          browser.tabs.remove(tab.id); /* kill the tab if it does */
+        }
+      });
+    }
+  });
+}  
 
 //onRemoved listener. fired when tab is removed
 browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
