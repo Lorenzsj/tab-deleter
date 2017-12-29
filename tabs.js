1d5ae2846efa64d1f0d76295c8bacbc1a48c9902
Lorenzsj <lorenzsj@clarkson.edu>
2017-12-29 03:29:24 -0500

    more efficient with some instead of forEach

--- tabs.js
+++ tabs.js
@@ -1,3 +1,4 @@
+/* the following two functions are for debugging and aren't necessary */
 function getCurrentWindowTabs() {
   return browser.tabs.query({currentWindow: true});
 }
@@ -27,10 +28,10 @@ function listTabs() {
   });
 }
 
-/* this will load the tab list everytime the plugin is opened */
+// this will load the tab list everytime the plugin is opened */
 document.addEventListener("DOMContentLoaded", listTabs);
 
-/* handle click events in plugin box */
+// handle click events in plugin box
 document.addEventListener("click", (e) => {
   function callOnActiveTab(callback) {
     getCurrentWindowTabs().then((tabs) => {
@@ -50,27 +51,32 @@ document.addEventListener("click", (e) => {
 });
 
 function massDelete() {
-  var blacklist = ["google.com", "bing.com", "yahoo.com"]; // load this from file or somethhing
+  var blacklist = ["google.com", "bing.com", "yahoo.com"]; // load this from a form, json file, or somethhing */
 
-  /* get all tabs that are in the current window of firefox */
+  // get all tabs that are in the current window of firefox
   browser.tabs.query({
     currentWindow: true
   }).then((tabs) => {
-    /* go through all tabs */
+    // go through all tabs
     for (var tab of tabs) {
-      /* check if the current tab has a substring from the blacklist array */
-      blacklist.forEach(function(site) {
+      // check if the current tab has a substring from the blacklist array
+      // some will short-circuit the loop if a match is found
+      blacklist.some(function(site) {
         if (tab.url.indexOf(site) !== -1) {
-          browser.tabs.remove(tab.id); /* kill the tab if it does */
+          browser.tabs.remove(tab.id); // kill the tab if it does
+          return true;
+        }
+        else {
+          console.log(tab.url + " does not contain " + site);
         }
       });
     }
   });
 }
 
-//onRemoved listener. fired when tab is removed
+// onRemoved listener. fired when tab is removed
 browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
-  c4onsole.log(`The tab with id: ${tabId}, is closing`);
+  console.log(`The tab with id: ${tabId}, is closing`);
 
   if(removeInfo.isWindowClosing) {
     console.log(`Its window is also closing.`);
